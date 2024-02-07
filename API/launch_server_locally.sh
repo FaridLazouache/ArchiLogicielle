8784b8623a8b86539ab19f986af535373d31098a
Farid <farid.lazouache@polytech-lille.net>
2024-02-07 16:23:38 +0100

    [FIX] file names

--- API/launch_server_locally.sh
+++ API/launch_server_locally.sh
@@ -1,11 +1,2 @@
-sudo pip-3 install uvicorn 
-sudo pip-3 install fastapi
-sudo pip-3 install psycopg2-binary
-export PATH=$PATH/usr/local/bin:
-curl https://raw.githubusercontent.com/FaridLazouache/ArchiLogici
-elle/develop/postgres/init.sql >> init.sql
-sudo yum update -y 
-sudo amazon-linux-extras install postgresql10 
-psql -h $DB_HOST -U postgres -f init.sql
-uvicorn main:app --reload --host $DB_HOST --port 5964
+uvicorn main:app --reload --host localhost --port 5964
 
