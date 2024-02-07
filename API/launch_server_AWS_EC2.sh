b725eac03625538927b281223c25112e18c9a805
Farid <farid.lazouache@polytech-lille.net>
2024-02-07 16:25:12 +0100

    [UPDATE] get index.html

--- API/launch_server_AWS_EC2.sh
+++ API/launch_server_AWS_EC2.sh
@@ -7,4 +7,6 @@ elle/develop/postgres/init.sql >> init.sql
 sudo yum update -y 
 sudo amazon-linux-extras install postgresql10 
 psql -h $DB_HOST -U postgres -f init.sql
+mkdir -p ../frontpage
+curl https://raw.githubusercontent.com/FaridLazouache/ArchiLogicielle/develop/frontpage/index.html >> ../frontpage/index.html
 uvicorn main:app --reload --host $DB_HOST --port 5964
