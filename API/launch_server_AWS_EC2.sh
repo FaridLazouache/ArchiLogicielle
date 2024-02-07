925a34d50d3bed74893f7572b743e7b5c06927eb
Farid <farid.lazouache@polytech-lille.net>
2024-02-07 17:16:43 +0100

    [FIX] Wrong paste

--- API/launch_server_AWS_EC2.sh
+++ API/launch_server_AWS_EC2.sh
@@ -2,8 +2,7 @@ sudo pip-3 install uvicorn
 sudo pip-3 install fastapi
 sudo pip-3 install psycopg2-binary
 export PATH=$PATH/usr/local/bin:
-curl https://raw.githubusercontent.com/FaridLazouache/ArchiLogici
-elle/develop/postgres/init.sql >> init.sql
+curl https://raw.githubusercontent.com/FaridLazouache/ArchiLogicielle/develop/postgres/init.sql >> init.sql
 sudo yum update -y 
 sudo amazon-linux-extras install postgresql10 
 psql -h $DB_HOST -U postgres -f init.sql
