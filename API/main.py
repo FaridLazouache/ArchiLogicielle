088545cd8369a274e264d52d64fc7cb5c3af04c1
Farid <farid.lazouache@polytech-lille.net>
2024-02-07 18:47:43 +0100

    [ADD] POST, PUT and DELETE feature to API

--- API/main.py
+++ API/main.py
@@ -1,5 +1,5 @@
-from typing import Union
-from fastapi import FastAPI
+from typing import Union, Optional
+from fastapi import FastAPI, HTTPException
 import psycopg2
 import os
 from starlette.responses import FileResponse
@@ -33,8 +33,72 @@ def display_all():
     cur.close()
     return {"data": output}
 
-# Add an article to the database
-# @app.get("/add/{name}")
-# def add_item(name: string):
-#     cur = conn.cursor()
-#     cur.execute("INSERT INTO article(id, name)"
+# Add an article
+@app.post("/add/{name}")
+def create_item(name: str):
+    try:
+        cur = conn.cursor()
+        cur.execute("INSERT INTO article (name) VALUES (%s);", (name,))
+        conn.commit()
+        cur.close()
+        return {"message": "Item created successfully"}
+    except Exception as e:
+        conn.rollback()
+        raise HTTPException(status_code=500, detail=str(e))
+
+# Modify an article    
+@app.put("/modify/{item_id}/")
+def modify_item(item_id: int, name: Optional[str] = None, price: Optional[float] = None, stock: Optional[int] = None, item_desc: Optional[str] = None):
+    try:
+        cur = conn.cursor()
+        query = "UPDATE article SET "
+        params = []
+        if name is not None:
+            query += "name = %s, "
+            params.append(name)
+        if price is not None:
+            query += "price = %s, "
+            params.append(price)
+        if stock is not None:
+            query += "stock = %s, "
+            params.append(stock)
+        if item_desc is not None:
+            query += "item_desc = %s, "
+            params.append(item_desc)
+        query = query.rstrip(', ')
+        query += " WHERE id = %s;"
+        params.append(item_id)
+        
+        cur.execute(query, params)
+        conn.commit()
+        cur.close()
+        return {"message": "Item modified successfully"}
+    except Exception as e:
+        conn.rollback()
+        raise HTTPException(status_code=500, detail=str(e))
+    
+# Delete an article
+@app.delete("/delete/{item_id}")
+def delete_item(item_id: str):
+    try:
+        cur = conn.cursor()
+        cur.execute("DELETE FROM article WHERE id=%s;", (item_id,))
+        conn.commit()
+        cur.close()
+        return {"message": "Item deleted successfully"}
+    except Exception as e:
+        conn.rollback()
+        raise HTTPException(status_code=500, detail=str(e))
+    
+# Delete every articles    
+@app.delete("/purge")
+def purge_database():
+    try:
+        cur = conn.cursor()
+        cur.execute("DELETE FROM article;")
+        conn.commit()
+        cur.close()
+        return {"message": "Database successfully purged"}
+    except Exception as e:
+        conn.rollback()
+        raise HTTPException(status_code=500, detail=str(e))
