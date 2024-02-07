fd523e25a394d18eff839b91805ca469c007ba61
Farid <farid.lazouache@polytech-lille.net>
2024-02-07 17:19:55 +0100

    [CHANGE] local DB password

--- postgres/docker_run.sh
+++ postgres/docker_run.sh
@@ -2,6 +2,6 @@ sudo docker stop database && sudo docker rm database
 #sudo docker image rm database_image
 #sudo docker build -t database_image .
 #sudo docker run -d -e POSTGRES_PASSWORD=root -p 5432:5432 --name database database_image
-sudo docker run -d -e POSTGRES_PASSWORD=root -p 5432:5432 --name database postgres
+sudo docker run -d -e POSTGRES_PASSWORD=postgresroot -p 5432:5432 --name database postgres
 #sudo docker cp init.sql database:/tmp/
 #sudo docker exec database /bin/bash psql -h localhost -U postgres -f init.sql
