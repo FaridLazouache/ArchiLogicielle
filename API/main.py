630660ab5207a307104954d6f48074a2f800a2d0
Farid <farid.lazouache@polytech-lille.net>
2024-02-07 16:14:03 +0100

    [UPDATE] API use environnment variable DB_HOST to connect to DB

--- API/main.py
+++ API/main.py
@@ -1,15 +1,19 @@
 from typing import Union
 from fastapi import FastAPI
 import psycopg2
+import os
 from starlette.responses import FileResponse
 
 app = FastAPI()
-conn = psycopg2.connect("dbname=magasin user=postgres host=localhost password=root ")
+DB_HOST = os.environ.get("DB_HOST")
+conn = psycopg2.connect(database="magasin", user="postgres", host=DB_HOST, password="postgresroot")
 
 # Launch login page
 @app.get("/login")
 def read_login():
     return FileResponse('../frontpage/index.html')
+
+# article(id SERIAL PRIMARY KEY, name VARCHAR(50) UNIQUE NOT NULL, price MONEY, stock INT, item_desc VARCHAR(100))
 
 # Display the selected article
 @app.get("/item/{item_id}")
@@ -28,3 +32,9 @@ def display_all():
     output = cur.fetchall()
     cur.close()
     return {"data": output}
+
+# Add an article to the database
+# @app.get("/add/{name}")
+# def add_item(name: string):
+#     cur = conn.cursor()
+#     cur.execute("INSERT INTO article(id, name)"
